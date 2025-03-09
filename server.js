53bedb815cae6b3edded22b055803bab4dc507e7
tugyy <coskuntugay6@gmail.com>
2025-03-09 21:44:07 +0300

    Add dotenv configuration for environment variable management

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+require("dotenv").config(); 
 
 const app = express();
 const server = http.createServer(app);
